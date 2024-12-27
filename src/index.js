7b6ec4e8e015150a8af3db713c6ced680af11513
ddangmul <j.ddangmul@gmail.com>
2024-12-28 04:36:47 +0900

    리덕스 저장소 store 리액트에 제공

--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux"; // 리덕스로부터 Provider import
 
-import './index.css';
-import App from './App';
+import "./index.css";
+import App from "./App";
+import store from "./store/index"; // 리덕스 저장소 import
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  // Provider에 store porp 값으로 저장소 설정 -> App 및 하위 컴포넌트는 저장소에 접근 가능
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
